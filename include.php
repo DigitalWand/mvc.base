8e78b20d477872e23ea77a1df539ff40b30298bc
Alexey Volkov <asgalex@gmail.com>
2020-05-21 21:12:44 +0300

    fix fatal "ComponentBuilder not found" error in wizard

--- include.php
+++ include.php
@@ -8,6 +8,6 @@ Loader::registerAutoLoadClasses('digitalwand.mvc',
         'DigitalWand\MVC\AjaxException' => 'lib/AjaxException.php',
         'DigitalWand\MVC\RestException' => 'lib/RestException.php',
         'DigitalWand\MVC\ActionTrait' => 'lib/ActionTrait.php',
-
+        'DigitalWand\MVC\ComponentBuilder' => 'lib/ComponentBuilder.php',
     )
 );
